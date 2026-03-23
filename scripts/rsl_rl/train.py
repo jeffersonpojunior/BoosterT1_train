6f4b9da79bc01325ff520f881f1c4bdb1742fb45
jeffersonpojunior <jpereira5541@gmail.com>
2026-03-23 18:04:01 -0300

    Export actor as TorchScript for deployment

--- scripts/rsl_rl/train.py
+++ scripts/rsl_rl/train.py
@@ -193,7 +193,8 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
 
     # run training
     runner.learn(num_learning_iterations=agent_cfg.max_iterations, init_at_random_ep_len=True)
-
+    runner.export_policy_as_jit(log_dir)
+    
     # close the simulator
     env.close()
 
